19e91fff3081c184e216a8fff977d02487cc3b78
Xiaochuan Dai <xiaochuandai@Xiaochuans-MacBook-Pro.local>
2020-06-08 23:10:19 -0400

    setting up react navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import 'react-native-gesture-handler';
 import React, {Component} from 'react';
 import {Text, StyleSheet, Button, View, Alert} from 'react-native';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {store, persistor} from './redux/store/store';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import LOGIN from './views/Login';
+
+const Stack = createStackNavigator();
+
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <LOGIN />
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen name="Home" component={LOGIN} />
+            </Stack.Navigator>
+          </NavigationContainer>
         </PersistGate>
       </Provider>
     );
