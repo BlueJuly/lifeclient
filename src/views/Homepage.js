6155ca540c0dc3a16c062da3f2591e3d0724899c
Xiaochuan Dai <xiaochuandai@Xiaochuans-MacBook-Pro.local>
2020-06-18 00:54:39 -0400

    update menu button active status

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View} from 'react-native';
 import {
   Container,
@@ -13,41 +13,54 @@ import {
 } from 'native-base';
 import {connect} from 'react-redux';
 
-export class Homepage extends Component {
-  render() {
-    return (
-      <Container>
-        <Header />
-        <Content />
-        <Footer>
-          <FooterTab>
-            <Button badge vertical>
-              <Badge success>
-                <Text>2</Text>
-              </Badge>
-              <Icon name="home" />
-              <Text>Pages</Text>
-            </Button>
-            <Button vertical>
-              <Icon name="calendar" />
-              <Text>Schedules</Text>
-            </Button>
-            <Button active badge vertical>
-              <Badge>
-                <Text>51</Text>
-              </Badge>
-              <Icon active name="navigate" />
-              <Text>Navigate</Text>
-            </Button>
-            <Button vertical>
-              <Icon name="person" />
-              <Text>Contact</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
-    );
-  }
+function Homepage() {
+  const [activeMenuButton, setActiveMenuButton] = useState(1);
+  return (
+    <Container>
+      <Header />
+      <Content />
+      <Footer>
+        <FooterTab>
+          <Button
+            active={activeMenuButton === 1}
+            onPress={() => setActiveMenuButton(1)}
+            badge
+            vertical>
+            <Badge success>
+              <Text>2</Text>
+            </Badge>
+            <Icon name="home" />
+            <Text>Pages</Text>
+          </Button>
+          <Button
+            active={activeMenuButton === 2}
+            onPress={() => setActiveMenuButton(2)}
+            vertical>
+            <Icon name="calendar" />
+            <Text>Schedules</Text>
+          </Button>
+          <Button
+            active={activeMenuButton === 3}
+            onPress={() => setActiveMenuButton(3)}
+            vertical>
+            <Icon name="person" />
+            <Text>Contact</Text>
+          </Button>
+          <Button
+            active={activeMenuButton === 4}
+            onPress={() => setActiveMenuButton(4)}
+            badge
+            vertical>
+            <Badge>
+              <Text>51</Text>
+            </Badge>
+            <Icon name="navigate" />
+            <Text>Navigate</Text>
+          </Button>
+        </FooterTab>
+      </Footer>
+    </Container>
+  );
 }
 
 const mapStateToProps = state => ({});
