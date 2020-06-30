fbdd42dfb6bd7f361071d515bcf4fbeecb7288e1
Xiaochuan Dai <xiaochuandai@Xiaochuans-MacBook-Pro.local>
2020-06-29 22:47:38 -0400

    make tile clickable

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {View, Image} from 'react-native';
+import {View, Image, TouchableOpacity} from 'react-native';
 
 import {
   Container,
@@ -27,47 +27,53 @@ function Homepage(props) {
     props.updateDeviceInfo();
     props.getUserTiles();
     console.log('----reducers in homepage is-----', props);
-  }, []);
+  }, [activeMenuButton]);
   return (
     <Container>
       <Header />
       <Content>
         {props.tilesReducer.tiles.map((tile, index) => {
           return (
-            <Card>
-              <CardItem>
-                <Left>
-                  <Thumbnail source={{uri: tile.profileImage.blobUrl}} />
+            <TouchableOpacity
+              key={index}
+              onPress={() => {
+                console.log('press me');
+              }}>
+              <Card>
+                <CardItem>
+                  <Left>
+                    <Thumbnail source={{uri: tile.profileImage.blobUrl}} />
+                    <Body>
+                      <Text>{tile.tileName}</Text>
+                      <Text note>GeekyAnts</Text>
+                    </Body>
+                  </Left>
+                </CardItem>
+                <CardItem cardBody>
+                  <Image
+                    source={{uri: tile.resource[0].blobUrl}}
+                    style={{height: 200, width: null, flex: 1}}
+                  />
+                </CardItem>
+                <CardItem>
+                  <Left>
+                    <Button transparent>
+                      <Icon active name="thumbs-up" />
+                      <Text>12 Likes</Text>
+                    </Button>
+                  </Left>
                   <Body>
-                    <Text>{tile.tileName}</Text>
-                    <Text note>GeekyAnts</Text>
+                    <Button transparent>
+                      <Icon active name="chatbubbles" />
+                      <Text>4 Comments</Text>
+                    </Button>
                   </Body>
-                </Left>
-              </CardItem>
-              <CardItem cardBody>
-                <Image
-                  source={{uri: tile.profileImage.blobUrl}}
-                  style={{height: 200, width: null, flex: 1}}
-                />
-              </CardItem>
-              <CardItem>
-                <Left>
-                  <Button transparent>
-                    <Icon active name="thumbs-up" />
-                    <Text>12 Likes</Text>
-                  </Button>
-                </Left>
-                <Body>
-                  <Button transparent>
-                    <Icon active name="chatbubbles" />
-                    <Text>4 Comments</Text>
-                  </Button>
-                </Body>
-                <Right>
-                  <Text>11h ago</Text>
-                </Right>
-              </CardItem>
-            </Card>
+                  <Right>
+                    <Text>11h ago</Text>
+                  </Right>
+                </CardItem>
+              </Card>
+            </TouchableOpacity>
           );
         })}
       </Content>
