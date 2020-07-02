1baf18c6a176f649240219550a1ead8d738d05ac
Xiaochuan Dai <xiaochuandai@Xiaochuans-MacBook-Pro.local>
2020-07-01 23:40:09 -0400

    make video playing work with android

--- src/views/VideoPlayer.js
+++ src/views/VideoPlayer.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, useRef} from 'react';
-import {Text, Button, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 import Video from 'react-native-video';
 import {connect} from 'react-redux';
 import {login} from '../redux/actions';
@@ -8,31 +8,39 @@ function VideoPlayer({route, navigation}) {
   const player = useRef(null);
   useEffect(() => {
     console.log('video player ref ------\n', player);
-    setTimeout(() => {
-      player.current.seek(30);
-    }, 3000);
+    // setTimeout(() => {
+    //   player.current.seek(3000);
+    // }, 3000);
   });
   function onBuffer() {}
   function videoError() {}
   return (
-    <Video
-      source={{uri: route.params.resource[0].blobUrl}} // Can be a URL or a local file.
-      // Store reference
-      ref={player}
-      onBuffer={onBuffer} // Callback when remote video is buffering
-      onError={videoError} // Callback when video cannot be loaded
-      style={styles.backgroundVideo}
-    />
+    <View style={styles.container}>
+      <Video
+        source={{uri: route.params.resource[0].blobUrl}} // Can be a URL or a local file.
+        // Store reference
+        ref={player}
+        onBuffer={onBuffer} // Callback when remote video is buffering
+        onError={videoError} // Callback when video cannot be loaded
+        style={styles.backgroundVideo}
+        controls={true}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   backgroundVideo: {
     position: 'absolute',
     top: 0,
     left: 0,
     bottom: 0,
     right: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 // const mapStateToProps = ({userReducer}) => {
