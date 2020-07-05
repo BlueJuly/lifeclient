dbd7ec0ac65e85154b17dd1785b0af8a131deab6
Xiaochuan Dai <xiaochuandai@Xiaochuans-MacBook-Pro.local>
2020-07-05 00:56:01 -0400

    add tabs for tiles

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -17,6 +17,8 @@ import {
   Left,
   Body,
   Right,
+  Tab,
+  Tabs
 } from 'native-base';
 import {connect} from 'react-redux';
 import {updateDeviceInfo} from '../redux/actions';
@@ -50,69 +52,79 @@ function Homepage(props) {
   return (
     <Container>
       <Header />
-      <Content>
-        <ImageView
-          images={tileImages}
-          imageIndex={0}
-          visible={imageVisible}
-          onRequestClose={() => setImageVisible(false)}
-        />
-        {props.tiles.map((tile, index) => {
-          return (
-            <TouchableOpacity
-              key={index}
-              onPress={() => {
-                if (tile.type === 'images') {
-                  openImageTile(tile);
-                }
-                if (tile.type === 'video') {
-                  openVideoTile(tile);
-                }
-                if (tile.type === 'documents') {
-                  openDocumentsTile(tile);
-                }
-                if (tile.type === 'website') {
-                  openWebViewTile(tile);
-                }
-              }}>
-              <Card>
-                <CardItem>
-                  <Left>
-                    <Thumbnail source={{uri: tile.profileImage.blobUrl}} />
-                    <Body>
-                      <Text>{tile.tileName}</Text>
-                      <Text note>GeekyAnts</Text>
-                    </Body>
-                  </Left>
-                </CardItem>
-                <CardItem cardBody>
-                  <Image
-                    source={{uri: tile.profileImage.blobUrl}}
-                    style={{height: 200, width: null, flex: 1}}
-                  />
-                </CardItem>
-                <CardItem>
-                  <Left>
-                    <Button transparent>
-                      <Icon active name="thumbs-up" />
-                      <Text>12 Likes</Text>
-                    </Button>
-                  </Left>
-                  <Body>
-                    <Button transparent>
-                      <Icon active name="chatbubbles" />
-                      <Text>4 Comments</Text>
-                    </Button>
-                  </Body>
-                  <Right>
-                    <Text>11h ago</Text>
-                  </Right>
-                </CardItem>
-              </Card>
-            </TouchableOpacity>
-          );
-        })}
-      </Content>
+      <Tabs>
+        <Tab heading="All Tiles">
+          <Content>
+            <ImageView
+              images={tileImages}
+              imageIndex={0}
+              visible={imageVisible}
+              onRequestClose={() => setImageVisible(false)}
+            />
+            {props.tiles.map((tile, index) => {
+              return (
+                <TouchableOpacity
+                  key={index}
+                  onPress={() => {
+                    if (tile.type === 'images') {
+                      openImageTile(tile);
+                    }
+                    if (tile.type === 'video') {
+                      openVideoTile(tile);
+                    }
+                    if (tile.type === 'documents') {
+                      openDocumentsTile(tile);
+                    }
+                    if (tile.type === 'website') {
+                      openWebViewTile(tile);
+                    }
+                  }}>
+                  <Card>
+                    <CardItem>
+                      <Left>
+                        <Thumbnail source={{uri: tile.profileImage.blobUrl}} />
+                        <Body>
+                          <Text>{tile.tileName}</Text>
+                          <Text note>GeekyAnts</Text>
+                        </Body>
+                      </Left>
+                    </CardItem>
+                    <CardItem cardBody>
+                      <Image
+                        source={{uri: tile.profileImage.blobUrl}}
+                        style={{height: 200, width: null, flex: 1}}
+                      />
+                    </CardItem>
+                    <CardItem>
+                      <Left>
+                        <Button transparent>
+                          <Icon active name="thumbs-up" />
+                          <Text>12 Likes</Text>
+                        </Button>
+                      </Left>
+                      <Body>
+                        <Button transparent>
+                          <Icon active name="chatbubbles" />
+                          <Text>4 Comments</Text>
+                        </Button>
+                      </Body>
+                      <Right>
+                        <Text>11h ago</Text>
+                      </Right>
+                    </CardItem>
+                  </Card>
+                </TouchableOpacity>
+              );
+            })}
+          </Content>
+        </Tab>
+        <Tab heading="Video Time">
+          <View>
+            <Text>Hello</Text>
+          </View>
+        </Tab>
+      </Tabs>
+
       <Footer>
         <FooterTab>
           <Button
