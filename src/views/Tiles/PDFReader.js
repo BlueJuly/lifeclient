dd5de414f076c23ed31cc5e3b64bb4af44a4a6f0
Xiaochuan Dai <xiaochuandai@Xiaochuans-MacBook-Pro.local>
2020-07-20 01:40:41 -0400

    add goback for PDF view

--- src/views/Tiles/PDFReader.js
+++ src/views/Tiles/PDFReader.js
@@ -1,6 +1,17 @@
 import React, {useState, useEffect, useRef} from 'react';
 import {Dimensions, View, StyleSheet} from 'react-native';
 import PDF from 'react-native-pdf';
+import {
+  Container,
+  Header,
+  Button,
+  Icon,
+  Title,
+  Left,
+  Body,
+  Right,
+  Subtitle,
+} from 'native-base';
 function PDFReader({route, navigation}) {
   const source = {
     uri: route.params.resource[0].blobUrl,
@@ -9,26 +20,42 @@ function PDFReader({route, navigation}) {
   useEffect(() => {
     console.log('------- PDF Component ------');
   });
-
+  function goBack() {
+    navigation.navigate('Homepage');
+  }
   return (
-    <View style={styles.container}>
-      <PDF
-        source={source}
-        onLoadComplete={(numberOfPages, filePath) => {
-          console.log(`number of pages: ${numberOfPages}`);
-        }}
-        onPageChanged={(page, numberOfPages) => {
-          console.log(`current page: ${page}`);
-        }}
-        onError={error => {
-          console.log(error);
-        }}
-        onPressLink={uri => {
-          console.log(`Link presse: ${uri}`);
-        }}
-        style={styles.pdf}
-      />
-    </View>
+    <Container>
+      <Header>
+        <Left>
+          <Button transparent onPress={() => goBack()}>
+            <Icon name="arrow-back" />
+          </Button>
+        </Left>
+        <Body>
+          <Title>Tiles</Title>
+          <Subtitle>Document</Subtitle>
+        </Body>
+        <Right />
+      </Header>
+      <View style={styles.container}>
+        <PDF
+          source={source}
+          onLoadComplete={(numberOfPages, filePath) => {
+            console.log(`number of pages: ${numberOfPages}`);
+          }}
+          onPageChanged={(page, numberOfPages) => {
+            console.log(`current page: ${page}`);
+          }}
+          onError={error => {
+            console.log(error);
+          }}
+          onPressLink={uri => {
+            console.log(`Link presse: ${uri}`);
+          }}
+          style={styles.pdf}
+        />
+      </View>
+    </Container>
   );
 }
 
